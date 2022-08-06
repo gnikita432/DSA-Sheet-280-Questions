28ae078084029f8540eae3eb24c68e1a3978f901
Nikita Anil Gupta <61223163+gnikita432@users.noreply.github.com>
2022-08-06 10:48:48 +0530

    78. Subsets

--- Day 9/78. Subsets.java
+++ Day 9/78. Subsets.java
@@ -1,5 +1,43 @@
 // 78. Subsets
 
+class Solution {
+    public List < List < Integer >> subsets(int[] nums) {
+
+        subset(nums.length, nums, 0, new ArrayList < > ());
+
+        return result;
+    }
+    
+    List < List < Integer >> result = new ArrayList < > ();
+
+    private void subset(int N, int[] nums, int idx, List < Integer > res) {
+
+        if (idx >= N) {
+            result.add(new ArrayList < > (res));
+            return;
+        }
+
+        //  does not work this two
+        // subset(N, nums, idx+1, res.add(nums[idx]));
+        // subset(N, nums, idx+1, res);
+
+        // main logic
+        // case 1 : we pick the element
+        res.add(nums[idx]);
+        subset(N, nums, idx + 1, res); // move ahead
+        res.remove(res.size() - 1);
+
+        // case 2 : we don't pick the element ( notice, we did not add the current element in our temporary list
+        subset(N, nums, idx + 1, res); // move ahead
+
+    }
+
+    // Time Complexity : 
+    // Space Complexity : 
+}
+
+
+
 
 // -------------------------------------------------------
 // class Solution {
@@ -22,12 +60,10 @@
 //                 if(rem == 1){
 //                     res.add(nums[j]);
 //                 }else{
-
 //                 }
 //             }
 //              result.add(res);
 //         }
-
 //         return result;
 //     }
 //         // Time Complexity : O(N * 2^N)
