23f66ea96edbf0949fdc0329abbd87849b1cd23b
Nikita Anil Gupta <61223163+gnikita432@users.noreply.github.com>
2022-10-04 09:42:10 +0530

    540. Single Element in a Sorted Array

--- Day 11 (Binary Search)/540. Single Element in a Sorted Array.java
+++ Day 11 (Binary Search)/540. Single Element in a Sorted Array.java
@@ -28,3 +28,36 @@ class Solution {
         return nums[nums.length-1];
     }
 }
+
+//Approach 3 binary search - O(log n)
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        
+        int start = 0, end = nums.length-1;
+        if(nums.length==1){
+            return nums[0];
+        }
+        while(start<end){
+            
+            int mid = start + (end - start)/2;
+            if(mid>0 && nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1]){
+                return nums[mid];
+            }
+           else if(mid%2==0 && nums[mid]==nums[mid+1]){
+                start = mid + 1;
+            }
+            else if(mid%2==0 && nums[mid]!=nums[mid+1]){
+                end = mid-1;
+            }
+            else if(mid>0 && mid%2!=0 && nums[mid]==nums[mid-1]){
+                start = mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+        return nums[start];
+    }
+}
+
+//Approach 4 count the frequency of element and return element with 1 frequency - O(n)
