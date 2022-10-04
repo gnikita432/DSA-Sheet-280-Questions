c1959f2ae7be0582486348f6e57f26142fc5b568
Nikita Anil Gupta <61223163+gnikita432@users.noreply.github.com>
2022-10-04 09:40:59 +0530

    540. Single Element in a Sorted Array

--- Day 11 (Binary Search)/540. Single Element in a Sorted Array.java
+++ Day 11 (Binary Search)/540. Single Element in a Sorted Array.java
@@ -10,3 +10,21 @@ class Solution {
         return ans;
     }
 }
+
+//Approach 2 ---------- O(n)
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        
+        // Approach 2: traverse and check
+        if(nums.length == 1) return nums[0];
+        int i = 0;
+        while(i<nums.length-1){
+            if(nums[i] != nums[i+1]){
+                return nums[i];
+            }else{
+                i+=2;
+            }
+        }
+        return nums[nums.length-1];
+    }
+}
