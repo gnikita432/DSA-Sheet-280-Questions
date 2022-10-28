8ac44f552f39331b7baa22bc69fb1e5fc81e1598
Nikita Anil Gupta <61223163+gnikita432@users.noreply.github.com>
2022-10-28 13:34:57 +0530

    225. Implement Stack using Queues

---
+++ Day 13 (Stack And Queue)/225. Implement Stack using Queues.java
@@ -0,0 +1,49 @@
+// 225. Implement Stack using Queues
+
+// Push efficient - POP and TOP -> O(2n)
+class MyStack {
+    Queue<Integer> mainQ;
+    Queue<Integer> helperQ;
+   
+    public MyStack() {
+        mainQ = new ArrayDeque<>();
+        helperQ = new ArrayDeque<>();  
+    }
+    
+    public void push(int x) {
+        mainQ.add(x);
+    }
+    
+    public int pop() {
+        while(mainQ.size()>1){
+            helperQ.add(mainQ.remove());
+        }
+        int tope = mainQ.remove();
+        while(helperQ.size()>0){
+            mainQ.add(helperQ.remove());
+        }
+        return tope;
+    }
+    
+    public int top() {
+         while(mainQ.size()>1){
+            helperQ.add(mainQ.remove());
+        }
+        int topelement = mainQ.peek();
+        return topelement;
+    }
+    
+    public boolean empty() {
+        if(mainQ.size()==0)return true;
+        else return false;
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
