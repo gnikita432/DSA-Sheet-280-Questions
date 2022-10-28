b18b323efd32ec38c7d79968d8d7e068306e18a2
Nikita Anil Gupta <61223163+gnikita432@users.noreply.github.com>
2022-10-28 13:39:54 +0530

    225. Implement Stack using Queues pop efficient

--- Day 13 (Stack And Queue)/225. Implement Stack using Queues.java
+++ Day 13 (Stack And Queue)/225. Implement Stack using Queues.java
@@ -1,5 +1,43 @@
 // 225. Implement Stack using Queues
 
+
+//TOP and POP Efficient and push will take O(2n)
+class MyStack {
+    Queue<Integer> mainQ;
+    Queue<Integer> helperQ;
+   
+    public MyStack() {
+        mainQ = new ArrayDeque<>();
+        helperQ = new ArrayDeque<>();  
+    }
+    
+    public void push(int x) {
+        while(mainQ.size()>0){
+            helperQ.add(mainQ.remove());
+        }
+        mainQ.add(x);
+        while(helperQ.size()>0){
+            mainQ.add(helperQ.remove());
+        }
+    }
+    
+    public int pop() {
+        return mainQ.remove();
+    }
+    
+    public int top() {
+        int topelement = mainQ.peek();
+        return topelement;
+    }
+    
+    public boolean empty() {
+        if(mainQ.size()==0)return true;
+        else return false;
+    }
+}
+
+
+
 // Push efficient - POP and TOP -> O(2n)
 class MyStack {
     Queue<Integer> mainQ;
