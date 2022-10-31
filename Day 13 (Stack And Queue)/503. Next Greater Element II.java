a462101b62c911979e97b620a49435267f0533ef
Nikita Anil Gupta <61223163+gnikita432@users.noreply.github.com>
2022-10-31 10:44:20 +0530

    503. Next Greater Element II (not so good approach)

--- Day 13 (Stack And Queue)/503. Next Greater Element II.java
+++ Day 13 (Stack And Queue)/503. Next Greater Element II.java
@@ -1,5 +1,36 @@
 // 503. Next Greater Element II
 
+
+//Apprach 2 : A little bit optimized but not so good approach
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int [] res = new int[nums.length];
+        Stack<Integer> st = new Stack<Integer>(); 
+        st.push(0);
+        int max = nums[0];
+        for(int i = 1; i< 2*nums.length-1 ; i++){
+            while(st.size()>0 && nums[st.peek()]<nums[i%nums.length]){
+                res[st.peek()]=nums[i%nums.length];
+                st.pop();
+            }
+            if(nums[i%nums.length]>max){
+                max = nums[i%nums.length];
+            }
+            st.push(i%nums.length);
+        }
+        while(st.size()>0 && nums[st.peek()]<max){  
+            st.pop();
+        } 
+        while(st.size()>0){
+        res[st.peek()] = -1;
+        st.pop();
+        }
+
+        return res;
+    }
+}
+
+
 // Approach 1: Brute force - traverse each element and for each element again traverse till that element(circular) to find greater element. 
 // ----------------------O(n^2)
 class Solution {
