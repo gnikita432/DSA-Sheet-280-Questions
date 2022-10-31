245550fe43bb8daf51026de2467af6f10447b2d2
Nikita Anil Gupta <61223163+gnikita432@users.noreply.github.com>
2022-10-31 08:48:16 +0530

    Next Greater Element Simple Basic from start

--- Day 13 (Stack And Queue)/Next Greater Element(Basic).java
+++ Day 13 (Stack And Queue)/Next Greater Element(Basic).java
@@ -1,5 +1,28 @@
 // Next Greater Element Simple Basic 
 
+//From Start
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int [] res = new int[nums.length];
+        Stack<Integer> st = new Stack<Integer>(); 
+        st.push(0);
+        for(int i = 0; i<nums.length; i++){
+            while(st.size()>0 && nums[st.peek()]<nums[i]){
+                res[st.peek()]=nums[i];
+                st.pop();
+            }
+            st.push(i);
+        }
+        while(st.size()>0){
+            res[st.peek()] = -1;
+            st.pop();
+        }
+
+        return res;
+    }
+}
+
+//From End
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
         int [] res = new int[nums.length];
