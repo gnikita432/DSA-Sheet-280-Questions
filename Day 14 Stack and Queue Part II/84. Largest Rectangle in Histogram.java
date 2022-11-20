55d3abf704b568aec79224dbd8a717e2ba440e57
Nikita Anil Gupta <61223163+gnikita432@users.noreply.github.com>
2022-11-20 13:14:12 +0530

    84. Largest Rectangle in Histogram

--- Day 14 Stack and Queue Part II/84. Largest Rectangle in Histogram.java
+++ Day 14 Stack and Queue Part II/84. Largest Rectangle in Histogram.java
@@ -1,5 +1,56 @@
 // 84. Largest Rectangle in Histogram
 
+// Approach 2 - stack
+// Find boundries of an element in nser and nsel and them calculate area from those width and height
+// Time complexity  : O(3n)
+// Space Complexity : O(2n)
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int area = 0, maxarea = 0;
+        int[] nser = nextSmallestElementRight(heights);
+        int[] nsel = nextSmallestElementLeft(heights);
+        for(int i = 0 ; i<heights.length; i++){
+            area = heights[i] * (nser[i]-nsel[i]-1);
+            if(area>maxarea)
+                maxarea = area;
+        }
+        return maxarea;
+    }
+    public int[] nextSmallestElementRight(int[] heights){
+        int[] nser = new int[heights.length];
+        Stack<Integer> st = new Stack<>();
+        for(int i = 0 ; i<heights.length; i++){
+            while(st.size()>0 && heights[st.peek()]>heights[i]){
+                nser[st.peek()] = i;
+                st.pop();
+            }
+            st.push(i);
+        }
+        while(st.size()>0){
+            nser[st.peek()] = heights.length;
+            st.pop();
+        }
+        return nser;
+    }
+    public int[] nextSmallestElementLeft(int[] heights){
+        int[] nsel = new int[heights.length];
+        Stack<Integer> st = new Stack<>();
+        for(int i = heights.length-1 ; i>=0; i--){
+            while(st.size()>0 && heights[st.peek()]>heights[i]){
+                nsel[st.peek()] = i;
+                st.pop();
+            }
+            st.push(i);
+        }
+        while(st.size()>0){
+            nsel[st.peek()] = -1;
+            st.pop();
+        }
+        return nsel;
+    }
+}
+
+
 // Approach 1 : TLE O(n^2) find all rectangles considering each height one by one
 
 class Solution {
