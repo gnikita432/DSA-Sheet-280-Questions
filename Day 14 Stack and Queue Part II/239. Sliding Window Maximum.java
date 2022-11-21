17815ef53e9846c79cab8f92c8dea6875da4a470
Nikita Anil Gupta <61223163+gnikita432@users.noreply.github.com>
2022-11-21 10:39:23 +0530

    239. Sliding Window Maximum

--- Day 14 Stack and Queue Part II/239. Sliding Window Maximum.java
+++ Day 14 Stack and Queue Part II/239. Sliding Window Maximum.java
@@ -1,5 +1,33 @@
 // 239. Sliding Window Maximum
 
+//Most Optimal Approach - DeQueue - Doubly linked list
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int[] res = new int[nums.length-(k-1)];
+        Deque<Integer> deque = new LinkedList<Integer>();
+  
+        for(int i = 0 ; i<nums.length; i++){
+            //remove numbers out of range k
+            while(!deque.isEmpty() && deque.peekFirst() == i-k){
+                deque.removeFirst();
+            }
+            //remove smaller numbers then nums[i] in k range as they are useless
+            while(!deque.isEmpty() && nums[deque.peekLast()]<nums[i]){
+                deque.removeLast();
+            }
+            //Adding current element
+           deque.addLast(i);
+           if(i>=k-1){
+               res[i-(k-1)]=nums[deque.peekFirst()];
+           }
+        }
+        return res;
+    }
+}
+// Time Complexity : O(2n)
+// Space Complexity : O(k)
+
+
 // Approach 1 - Naive Solution - TLE - O(n*k)
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
