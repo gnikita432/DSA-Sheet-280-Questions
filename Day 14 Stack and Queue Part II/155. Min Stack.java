03d572d6b66e8bd6aef055c62c0054445845d81a
Nikita Anil Gupta <61223163+gnikita432@users.noreply.github.com>
2022-11-22 12:39:12 +0530

    155. Min Stack - optimal solution

--- Day 14 Stack and Queue Part II/155. Min Stack.java
+++ Day 14 Stack and Queue Part II/155. Min Stack.java
@@ -1,5 +1,51 @@
 // 155. Min Stack
 
+//Optimum Solution - space - O(n)
+class MinStack {
+
+    Stack<Long> st;
+    Long min = Long.MAX_VALUE;
+    public MinStack() {
+        st = new Stack<>();
+    }
+    
+    public void push(int value) {
+       Long val = Long.valueOf(value);
+        if(st.isEmpty()){
+            st.push(val);
+            min = val;
+        }else if(min>val){
+            st.push(val+val - min);
+            min = val;
+        }else{
+            st.push(val);
+        }
+    }
+    
+    public void pop() {
+        if(st.peek()<min){
+            Long val = st.pop();
+            min = (2*min)-val;
+        }else{
+            st.pop();
+        }
+    }
+    
+    public int top() {
+        if(st.peek()<min){
+            return min.intValue();
+        }else{
+            return st.peek().intValue();
+        }
+    }
+    
+    public int getMin() {
+        return min.intValue();
+    }
+}
+
+
+//Brute force - space- O(2n)
 class MinStack {
 
     class minValPair{
@@ -47,4 +93,3 @@ class MinStack {
  * int param_3 = obj.top();
  * int param_4 = obj.getMin();
  */
- 
