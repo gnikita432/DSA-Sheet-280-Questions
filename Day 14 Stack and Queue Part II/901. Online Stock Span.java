1a5ffe19eec486ea4dd5b28ece1484668cf74404
Nikita Anil Gupta <61223163+gnikita432@users.noreply.github.com>
2022-11-30 15:14:39 +0530

    901. Online Stock Span

---
+++ Day 14 Stack and Queue Part II/901. Online Stock Span.java
@@ -0,0 +1,25 @@
+// 901. Online Stock Span
+
+class StockSpanner {
+
+    ArrayList<Integer> stock;    
+    public StockSpanner() {
+        stock = new ArrayList<Integer>();
+    }
+    
+    public int next(int price) {
+        int p = 1;
+        for(int i = stock.size()-1 ; i>=0 && stock.get(i)<=price ; i--){
+            p++;
+        }
+        stock.add(price);
+        return p;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
+ 
