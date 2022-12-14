d530264fa6d030d83a0db25a365cbf7685f60a8f
Nikita Anil Gupta <61223163+gnikita432@users.noreply.github.com>
2022-12-14 23:10:24 +0530

    239. Sliding Window Maximum

--- Day 14 Stack and Queue Part II/239. Sliding Window Maximum.java
+++ Day 14 Stack and Queue Part II/239. Sliding Window Maximum.java
@@ -44,3 +44,44 @@ class Solution {
         return res;
     }
 }
+
+//Approach 3 - Stack solution - real nge
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        
+        //next greater element in right
+        Stack <Integer> st = new Stack();
+        int [] nge = new int[nums.length];
+        st.push(0);
+        
+        for(int i =1;i<nums.length;i++){
+           while(st.size()>0 && nums[i]>nums[st.peek()]){
+                   int pos = st.peek();
+                   nge[pos]=i;
+                   st.pop();
+            }
+          
+            st.push(i);
+        }
+        while(st.size()>0){
+                  int pos = st.peek();
+                   nge[pos]=nums.length;
+                   st.pop();
+            }
+
+     int [] max = new int[(nums.length-k)+1];
+        int j=0;
+        for(int i=0; i<=nums.length-k; i++){
+            if(j<i){
+                j=i;
+            } 
+            while(nge[j]<i+k){
+                j=nge[j];
+            }
+            
+            max[i]=nums[j];
+        }
+        
+        return max;
+    }
+}
