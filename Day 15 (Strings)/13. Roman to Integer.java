8f55a33fe602dbf572c580297127e5aed5bec3ca
Nikita Anil Gupta <61223163+gnikita432@users.noreply.github.com>
2022-12-18 23:13:47 +0530

    13. Roman to Integer

---
+++ Day 15 (Strings)/13. Roman to Integer.java
@@ -0,0 +1,21 @@
+// 13. Roman to Integer
+
+class Solution {
+    public int romanToInt(String s) {
+        HashMap<Character, Integer> m = new HashMap<Character, Integer>();
+        m.put('I',1);
+        m.put('V',5);
+        m.put('X',10);
+        m.put('L',50);
+        m.put('C',100);
+        m.put('D',500);
+        m.put('M',1000);
+        char [] c = s.toCharArray();
+        int [] n = new int[c.length];
+        for(int i=0;i<n.length;i++)n[i]=m.get(c[i]);
+        int sum=0;
+        for(int i=0;i<n.length;i++)sum = i==c.length-1||n[i]>=n[i+1]?sum+n[i]:sum-n[i];
+        return sum;
+        
+    }
+}
