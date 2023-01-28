1e9ceedbd3a16094c063bc1aa67cf2e780867b55
Nikita Anil Gupta <61223163+gnikita432@users.noreply.github.com>
2023-01-28 14:32:50 +0530

    z function

--- Day 16 (Strings part II)/28. Find the Index of the First Occurrence in a String.java
+++ Day 16 (Strings part II)/28. Find the Index of the First Occurrence in a String.java
@@ -1,5 +1,45 @@
 // 28. Find the Index of the First Occurrence in a String
 
+//z function O(n+m)
+
+// This is eventually n*m only but i tried impleting z function bellow is the better version of it
+class Solution {
+    public int strStr(String haystack, String needle) {
+        // step 1: concat the haystack and needle
+        String str = needle + "$" + haystack;
+
+        // step 2: keep one pointer i at 0 and j at needle+1 index
+        int i = 0, j = needle.length()+1 , count=needle.length()+2 , idx=-1;
+
+        // step 3: loop through
+        while(i< needle.length() && j< str.length()){
+            char nc = str.charAt(i);
+            char hsc = str.charAt(j);
+
+            // step 4: Compare
+
+            //if equals then move both and compares next element
+            if(nc == hsc){
+                i++;
+                j++;
+            }
+            
+            //if not equals then start again move i to first index and j to its second element and count will move one by one so it will be the next
+            else{
+                i=0;
+                j = count;
+                count++;
+            }
+          
+        }
+        if(str.charAt(i) == '$'){
+            idx = j - 2*(needle.length())-1;
+        }
+       return idx;
+    }
+}
+
+// Brute force O(n*m)
 class Solution {
     public int strStr(String haystack, String needle) {
         if(needle.length()>haystack.length()){
@@ -27,3 +67,5 @@ class Solution {
         return idx;
     }
 }
+
+//There is also a KMP Algorithm with O(n+m) time complexity
